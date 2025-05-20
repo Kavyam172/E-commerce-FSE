5a79738e40a073280890fd42eddc9749819ac4f6
BowlPulp <kartikarora5832@gmail.com>
2025-05-20 10:54:23 +0530

    refactor: Comment out social login options in the Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -267,14 +267,14 @@ const Login = () => {
               </div>
             </form>
             
-            <div className="relative flex items-center justify-center mt-8">
+            {/* <div className="relative flex items-center justify-center mt-8">
               <div className="absolute border-t border-gray-300 dark:border-gray-700 w-full"></div>
               <div className="relative bg-white dark:bg-gray-800 px-4 text-sm text-gray-500 dark:text-gray-400">
                 Or continue with
               </div>
-            </div>
+            </div> */}
             
-            <div className="grid grid-cols-2 gap-4 mt-6">
+            {/* <div className="grid grid-cols-2 gap-4 mt-6">
               <button className="flex items-center justify-center gap-2 py-2.5 border border-gray-300 dark:border-gray-700 rounded-md hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="text-[#4285F4]" viewBox="0 0 16 16">
                   <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
@@ -287,7 +287,7 @@ const Login = () => {
                 </svg>
                 Facebook
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
